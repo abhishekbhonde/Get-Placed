607e2d74aacb337c5e61bf0ae0bba322347cb951
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-08-24 18:22:02 +0530

    pattern codes

---
+++ java/classcode/Pattern/pattern2.java
@@ -0,0 +1,11 @@
+class pattern2{
+	public static void main(String [] args){
+		int n =3;
+		for (int i = 1; i<=n; i++){
+			for(int j = 1; j<=n; j++){
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ java/classcode/Pattern/pattern3.java
@@ -0,0 +1,10 @@
+class pattern3{
+	public static void main(String [] args){
+		for(int i =1; i<=3;i++){
+			for(int j=1; j<=3; j++){
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
