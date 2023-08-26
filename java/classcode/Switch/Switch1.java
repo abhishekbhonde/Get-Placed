a34a913d5a9ed15e95667e37eba2d2ab7dfe5654
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-08-27 01:08:13 +0530

    Switch

---
+++ java/classcode/Switch/Switch1.java
@@ -0,0 +1,25 @@
+class Switch1{
+	public static void main(String [] args){
+
+		int x = 5;
+
+		switch(x){
+
+			case 1: 
+				System.out.println("One");
+				
+			case 2: 
+				System.out.println("Two");
+			case 3:
+                                System.out.println("Three");
+                                
+                        case 4: 
+                                System.out.println("Four");
+			case 5: 
+				System.out.println("Five");
+
+			default :
+				System.out.println("After Switch");
+		}
+	}
+}
---
+++ java/classcode/Switch/switch.java
@@ -0,0 +1,3 @@
+class switch2{
+	public static void main(String [] args){
+
