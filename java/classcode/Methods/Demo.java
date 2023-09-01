5472be96314fa6a2e7bb2fd9bf2ba727b2b4808c
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-09-01 22:27:15 +0530

    added

---
+++ java/classcode/Methods/Demo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Demo{
+    public static void main(String[] args) {
+        
+	fun();
+	gun();
+    }
+
+  static  void fun(){
+        System.out.println("This is fun method");
+    }
+
+ static void gun(){
+	  System.out.println("This is gun method");
+  }
+}
